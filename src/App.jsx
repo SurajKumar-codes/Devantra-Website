b1fcebad62e626cc76a6672625922d509b6afc07
Suraj <s.surajkushawaha@gmail.com>
2025-11-10 21:19:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen w-screen text-white bg-gray-950 text-center flex flex-col justify-center items-center gap-5'>
+        <h1 className='text-4xl font-bold'>Hello Everyone, Start Your Project </h1>
+        
+        <p className='text-xl'>Use Tailwindcss and javascript in this project </p>
+      </div>
+    </>
+  )
+}
+
+export default App
